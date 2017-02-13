0cdd5e362b0a9ce4776c9d3d4fc71ac590390527
Titan <howay.tan@gmail.com>
2017-02-13 15:11:51 +0800

    Add PlanGroup

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,13 @@ export interface Plan {
     id: number;
     title: string;
     description: string;
+    optional: boolean;
+}
+export interface PlanGroup {
+    id: string;
+    title: string;
+    description: string;
+    mask: number;
+    discount: number;
+    plans: Plan[];
 }
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,12 @@ export interface Plan {
   description: string;
   optional: boolean;
 };
+
+export interface PlanGroup {
+  id: string;
+  title: string;
+  description: string;
+  mask: number;
+  discount: number;
+  plans: Plan[];
+}
