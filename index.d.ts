d8d9c9c35f078ce319729072c08a22f8ebb672fc
Titan <howay.tan@gmail.com>
2017-03-02 15:48:00 +0800

    Remove description of plan and plan groups

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,11 @@
 export interface Plan {
     id: number;
     title: string;
-    description: string;
     optional: boolean;
 }
 export interface PlanGroup {
     id: string;
     title: string;
-    description: string;
     mask: number;
     discount: number;
     plans: Plan[];
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 export interface Plan {
   id: number;
   title: string;
-  description: string;
   optional: boolean;
 };
 
 export interface PlanGroup {
   id: string;
   title: string;
-  description: string;
   mask: number;
   discount: number;
   plans: Plan[];
